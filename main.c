cbdb2e0313fa22c483a31bf505c324ba8ef8659f
NikitaVolia <volanskijnikita2@gmail.com>
2025-10-06 12:32:45 +0200

    Add vector2i.h file with vector2i struct and functions

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+#include"vector2i.h"
+
+int math_function(int value) {
+    return value * 2 + 3;
+}
 
 int main() {
+    vector2i vector;
+
+    vector = create_vector2i(10, -2);
+    print_vector2i(vector);
+    
+    vector = foreach_vector2i(vector, math_function);
+    print_vector2i(vector);
 
     exit(EXIT_SUCCESS);
 }
