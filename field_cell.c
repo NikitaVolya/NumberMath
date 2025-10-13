1bfc688ad73ecb8c5e2fd6895030a56e137c1f1a
NikitaVolia <volanskijnikita2@gmail.com>
2025-10-13 23:12:12 +0200

    Change color order to display field_cell

--- field_cell.c
+++ field_cell.c
@@ -17,10 +17,10 @@ field_cell create_field_cell(short value) {
 void display_field_cell(field_cell cell) {
     const char *color;
 
-    if (!cell.is_available)
-        color = UNENABLE_COLOR;
-    else if (cell.is_highlited)
+    if (cell.is_highlited)
         color = HIGHLITED_COLOR;
+    else if (!cell.is_available)
+        color = UNENABLE_COLOR;
     else
         color = ENABLE_COLOR;
 
