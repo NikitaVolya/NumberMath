da2e9710cd54543830db34aa7c28b5a63b4fa0ac
mm01609z <mm01609z@etumet104033-x>
2025-10-14 09:20:23 +0200

    Add functionality in game_menu

--- game_menu.h
+++ game_menu.h
@@ -2,14 +2,34 @@
 #include <string.h>
 #include "game.h" 
 
+int execute(int position, int *exit) {
+    int res = 1;
+    switch (position) {
+    case 0:
+        start_game();
+        break;
+    case 1:
+        break;
+    case 2:
+        *exit = 1;
+        break;
+    default:
+        res = 0;
+        break;
+    }
+    return res;
+}
+        
+    
 int game_menu() {
     const char *items[] = { "New Game", "Load Game", "Exit" };
     const int n = 3;
     int sel = 0;
     GAME_KEY key;
     int i;
+    int exit = 0;
 
-    while (1) {
+    while (!exit) {
         if (system("clear") != 0)
             printf("Error while console clearing\n");
         printf("=== GAME MENU ===\n\n");
@@ -22,14 +42,22 @@ int game_menu() {
         fflush(stdout);
 
         key = get_game_key();
-        if(key == UP) {
+        switch (key) {
+        case UP:
             sel = (sel + n - 1) % n;
-        }
-        else if(key == DOWN) {
+            break;
+        case DOWN:
             sel = (sel + 1) % n;
+            break;
+        case ENTER:
+            execute(sel, &exit);
+            break;
+        default:
+            break;
         }
     }
     printf("Goodbye!\n");
     return 0;
 }
+
 
