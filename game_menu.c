0428c6ef56b05e0eb01c0c6e886f29b96b3dff4d
Aurora-Borealiss <125693084+Aurora-Borealiss@users.noreply.github.com>
2025-10-21 01:40:21 +0200

    Add tutorial function in game_menu
    Added a tutorial feature to guide players on game mechanics.

--- game_menu.c
+++ game_menu.c
@@ -4,6 +4,47 @@
 
 #include "game_menu.h"
 
+void show_tutorial() {
+    const char *tutorial_texts[] = {
+        "Bienvenue dans Number Match!\n\nUtilisez les fleches pour naviguer dans le menu et ENTER pour quitter le Tutorial",
+        "Le but est d'effacer tous les chiffres du plateau.\n\nTrouvez des paires de nombres égaux (1 et 1, 7 et 7)\n\nou des paires dont la somme est égale à 10 (6 et 4, 8 et 2) sur la grille numérique.\n\nTouchez les nombres un par un pour les rayer et gagner des points.",
+        "Fonctionnalité: S’il ne reste plus de chiffres sur une ligne, celle-ci sera entièrement supprimée et le reste sera fusionné.\n\nVérifiez différentes directions : les paires peuvent être horizontales, verticales ou même diagonales.\n\nRecherchez les chiffres séparés par des cases vides : les chiffres placés en diagonale opposée peuvent également former des paires.\n\nVérifiez ligne par ligne : examinez la fin d’une ligne à droite et le début de la ligne suivante à gauche, il peut y avoir des paires.\n\n",
+        "Bonne chance et amusez-vous bien!"
+    };
+    int n = 4;
+    int page = 0;
+    GAME_KEY key;
+    int done = 0;
+
+    while (!done) {
+        if (system("clear") != 0)
+            printf("Error while console clearing\n");
+
+        printf("\n==================== TUTORIAL ====================\n\n");
+        printf("%s\n", tutorial_texts[page]);
+        printf("\n=================================================\n");
+        printf("Page %d/%d\n", page + 1, n);
+        printf("Utilisez LEFT / RIGHT pour naviguer, ENTER pour quitter.\n");
+        printf("=================================================\n");
+        fflush(stdout);
+
+        key = get_game_key();
+        switch (key) {
+        case LEFT:
+            page = (page + n - 1) % n;
+            break;
+        case RIGHT:
+            page = (page + 1) % n;
+            break;
+        case ENTER:
+            done = 1;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
 int execute(int position, int *exit) {
     int res = 1;
     switch (position) {
@@ -15,6 +56,9 @@ int execute(int position, int *exit) {
     case 1:
         break;
     case 2:
+        show_tutorial();
+        break;
+    case 3:
         *exit = 1;
         break;
     default:
@@ -25,8 +69,8 @@ int execute(int position, int *exit) {
 }
 
 int game_menu() {
-    const char *items[] = { "New Game", "Load Game", "Exit" };
-    const int n = 3;
+    const char *items[] = { "New Game", "Load Game", "Tutorial", "Exit" };
+    const int n = 4;
     int sel = 0;
     GAME_KEY key;
     int i;
