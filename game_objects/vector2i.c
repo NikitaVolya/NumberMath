1aad4893a4ac7a2a116630b07241ce7a4a4a9c2f
NikitaVolia <volanskijnikita2@gmail.com>
2025-10-26 00:58:58 +0200

    Change commets in vector2i

--- game_objects/vector2i.c
+++ game_objects/vector2i.c
@@ -1,10 +1,6 @@
 #include"vector2i.h"
 
-/* 
-    Creates a 2D integer vector.
-    input:    x, y - integer coordinates
-    output:   vector2i structure with given coordinates
-*/
+
 vector2i create_vector2i(int x, int y) {
     vector2i vector;
     vector.x = x;
@@ -12,22 +8,10 @@ vector2i create_vector2i(int x, int y) {
     return vector;
 }
 
-/* 
-    Creates a 2D integer vector with both coordinates set to zero.
-
-    input:    none
-    output:   vector2i(0, 0)
-*/
 vector2i create_zero_vector2i() {
     return create_vector2i(0, 0);
 }
 
-/* 
-    Adds two 2D integer vectors.
-
-    input:    a, b - vectors to add
-    output:   sum of the two vectors
-*/
 vector2i add_vector2i(vector2i a, vector2i b) {
     vector2i rep;
     rep.x = a.x + b.x;
@@ -35,12 +19,6 @@ vector2i add_vector2i(vector2i a, vector2i b) {
     return rep;
 }
 
-/* 
-    Subtracts one 2D integer vector from another.
-
-    input:    a, b - vectors to subtract (a - b)
-    output:   result of subtraction
-*/
 vector2i sub_vector2i(vector2i a, vector2i b) {
     vector2i rep;
     rep.x = a.x - b.x;
@@ -48,13 +26,6 @@ vector2i sub_vector2i(vector2i a, vector2i b) {
     return rep;
 }
 
-/* 
-    Multiplies a 2D integer vector by an integer scalar.
-
-    input:    vector - source vector
-              value  - scalar multiplier
-    output:   scaled vector
-*/
 vector2i mult_vector2i(vector2i vector, int value) {
     vector2i rep;
     rep.x = vector.x * value;
@@ -62,12 +33,7 @@ vector2i mult_vector2i(vector2i vector, int value) {
     return rep;
 }
 
-/* 
-    Returns a vector with absolute values of both coordinates.
 
-    input:    vector - source vector
-    output:   vector with absolute coordinates
-*/
 vector2i abs_vector2i(vector2i vector) {
     vector2i rep;
     rep.x = (vector.x < 0 ? -vector.x : vector.x);
@@ -75,13 +41,6 @@ vector2i abs_vector2i(vector2i vector) {
     return rep;
 }
 
-/* 
-    Applies a function to each component of a 2D integer vector and returns the result.
-
-    input:    vector - source vector
-              fptr   - pointer to a function that takes and returns an int
-    output:   new vector with transformed components
-*/
 vector2i foreach_vector2i(vector2i vector, int(*fptr)(int)) {
     vector2i rep;
     rep.x = fptr(vector.x);
