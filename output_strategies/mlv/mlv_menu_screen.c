786387532e2df9f005e9d1bf5b361beb8cdf8a37
mm01609z <mm01609z@etumet104033>
2025-11-27 10:54:58 +0100

    Menu Modification

--- output_strategies/mlv/mlv_menu_screen.c
+++ output_strategies/mlv/mlv_menu_screen.c
@@ -4,9 +4,9 @@
 #define BTN_W  300
 #define BTN_H   80
 #define BTN_COLOR      MLV_rgba(70,130,255,255)
-#define BTN_HOVER      MLV_rgba(120,165,255,255)
+#define BTN_HOVER      MLV_rgba(255, 223, 15, 200)
 #define TEXT_COLOR     MLV_COLOR_WHITE
-#define BG_COLOR       MLV_rgba(255,247,229,255)
+#define BG_COLOR       MLV_rgba(44, 170, 201, 255)
 
 static int hit(int mx,int my,int x,int y){
     return mx>=x && mx<=x+BTN_W && my>=y && my<=y+BTN_H;
@@ -17,7 +17,7 @@ static void draw_button(const char *label,int y,int hover){
     int x = (GAME_WINDOW_WIDTCH - BTN_W)/2;
 
     MLV_draw_filled_rectangle(x,y,BTN_W,BTN_H, hover?BTN_HOVER:BTN_COLOR);
-    MLV_draw_rectangle(x,y,BTN_W,BTN_H,MLV_COLOR_BLACK);
+    MLV_draw_rectangle(x,y,BTN_W,BTN_H,MLV_COLOR_BLUE);
 
     MLV_get_size_of_text(label,&tw,&th);
     MLV_draw_text(x+BTN_W/2-tw/2, y+BTN_H/2-th/2, label, TEXT_COLOR);
@@ -27,6 +27,7 @@ void mlv_show_menu(struct game_config *config){
 
     int mx,my;
     int running = 1;
+    int tw,th;
 
     int play_y=230, load_y=330, tut_y=430, quit_y=530;
 
@@ -39,9 +40,8 @@ void mlv_show_menu(struct game_config *config){
         MLV_clear_window(BG_COLOR);
 
         {
-            int tw,th;
             MLV_get_size_of_text("NUMBER MATCH",&tw,&th);
-            MLV_draw_text(GAME_WINDOW_WIDTCH/2-tw/2,120,"NUMBER MATCH",MLV_COLOR_BLACK);
+            MLV_draw_text(GAME_WINDOW_WIDTCH/2-tw/2,120,"NUMBER MATCH",MLV_COLOR_WHITE);
         }
 
         /* DRAW ONLY — NO ACTIONS */
