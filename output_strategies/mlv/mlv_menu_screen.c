12ac8312d67883d0b19e339dbd04ee4ff642b39f
mm01609z <mm01609z@etumet104033-x>
2025-11-27 15:11:19 +0100

    MENU BACKGROUND FADE

--- output_strategies/mlv/mlv_menu_screen.c
+++ output_strategies/mlv/mlv_menu_screen.c
@@ -3,23 +3,35 @@
 
 #define BTN_W  300
 #define BTN_H   80
-#define BTN_COLOR      MLV_rgba(70,130,255,255)
-#define BTN_HOVER      MLV_rgba(255, 223, 15, 200)
-#define TEXT_COLOR     MLV_COLOR_WHITE
+#define BTN_R   18
+#define BASE_R 255
+#define BASE_G 247
+#define BASE_B 229
+#define BTN_COLOR      MLV_rgba(0,0,128, 255)
+#define BTN_HOVER      MLV_rgba(70,130,255,255)
+#define TEXT_COLOR     MLV_COLOR_BLUE
 #define BG_COLOR       MLV_rgba(44, 170, 201, 255)
 
 static int hit(int mx,int my,int x,int y){
     return mx>=x && mx<=x+BTN_W && my>=y && my<=y+BTN_H;
 }
 
+static void draw_round_button(int x, int y, int w, int h, int r, MLV_Color c) {
+    MLV_draw_filled_rectangle(x+r, y, w-2*r, h, c);
+    MLV_draw_filled_rectangle(x, y+r, w, h-2*r, c);
+
+    MLV_draw_filled_circle(x+r, y+r, r, c);
+    MLV_draw_filled_circle(x+w-r, y+r, r, c);
+    MLV_draw_filled_circle(x+r, y+h-r, r, c);
+    MLV_draw_filled_circle(x+w-r, y+h-r, r, c);
+}
+
 static void draw_button(const char *label,int y,int hover){
     int tw,th;
     int x = (GAME_WINDOW_WIDTCH - BTN_W)/2;
 
-    MLV_draw_filled_rectangle(x,y,BTN_W,BTN_H, hover?BTN_HOVER:BTN_COLOR);
-    MLV_draw_rectangle(x,y,BTN_W,BTN_H,MLV_COLOR_BLUE);
-
-    MLV_get_size_of_text(label,&tw,&th);
+    draw_round_button(x, y, BTN_W, BTN_H, BTN_R, hover?BTN_HOVER:BG_COLOR);
+    MLV_get_size_of_text(label, &tw, &th);
     MLV_draw_text(x+BTN_W/2-tw/2, y+BTN_H/2-th/2, label, TEXT_COLOR);
 }
 
@@ -29,6 +41,9 @@ void mlv_show_menu(struct game_config *config){
     int running = 1;
     int tw,th;
 
+    float fade = 0.001f;
+    int direction = 1;
+    
     int play_y=230, load_y=330, tut_y=430, quit_y=530;
 
     MLV_create_window("NumberMatch Menu","NumberMatch",
@@ -37,11 +52,20 @@ void mlv_show_menu(struct game_config *config){
     while(running){
 
         MLV_get_mouse_position(&mx,&my);
-        MLV_clear_window(BG_COLOR);
 
+        fade += 0.003f * direction;
+        if(fade >= 1.0f)
+            direction = -1;
+        if(fade <= 0.0f)
+            direction = 1;
+        int r = BASE_R - (int)        (fade*30);
+        int g = BASE_G - (int)        (fade*5);
+        int b = BASE_B - (int)        (fade*30);
+
+        MLV_clear_window( MLV_rgba(r,g,b,255) );
         {
             MLV_get_size_of_text("NUMBER MATCH",&tw,&th);
-            MLV_draw_text(GAME_WINDOW_WIDTCH/2-tw/2,120,"NUMBER MATCH",MLV_COLOR_WHITE);
+            MLV_draw_text(GAME_WINDOW_WIDTCH/2-tw/2,120,"NUMBER MATCH",TEXT_COLOR);
         }
 
         /* DRAW ONLY — NO ACTIONS */
